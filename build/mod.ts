000a39dd77a7c7aa9eb19ed0dd114f9c25ca096a
Nbiba Bedis <bedisnbiba@gmail.com>
2024-12-15 14:59:55 +0100

    prepare for deploy

---
+++ build/mod.ts
@@ -0,0 +1,10 @@
+import { serveFile } from "jsr:@std/http@1.0.12/file-server";
+
+Deno.serve((req) => {
+  const url = req.url;
+  const path = new URL(url).pathname;
+  if (path === "/") {
+    return serveFile(req, "dist/index.html");
+  }
+  return serveFile(req, `dist/${path.slice(1)}`); // remove leading slash
+});
