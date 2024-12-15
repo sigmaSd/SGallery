735274e8b9db33fdfdde6a32c6b6978c853dd895
Nbiba Bedis <bedisnbiba@gmail.com>
2024-12-15 15:08:15 +0100

    debug

--- build/mod.ts
+++ build/mod.ts
@@ -1,6 +1,10 @@
 import { serveFile } from "jsr:@std/http@1.0.12/file-server";
 
+console.log([...Deno.readDirSync(".")]);
+console.log(4);
+console.log([...Deno.readDirSync("..")]);
 Deno.serve((req) => {
+  console.log(req);
   const url = req.url;
   const path = new URL(url).pathname;
   if (path === "/") {
